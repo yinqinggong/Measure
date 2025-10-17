72f044d76c3b5e9b19adffc9ea8d17c93e6a690b
yinqinggong <yinqinggong@126.com>
2025-10-17 15:15:51 +0800

    修改相机坐标图片

--- Measure/MeasureDlg.cpp
+++ Measure/MeasureDlg.cpp
@@ -1549,11 +1549,11 @@ LRESULT CMeasureDlg::OnUserMessageRecMerge(WPARAM wParam, LPARAM lParam)
 		log_scale_merge(results1, results2, r_mat32, t_mat32, kX_OFFSET, kY_OFFSET, kZ_OFFSET, kPOS_EPS, flags1, flags2);
 		
 		//保存3D图片
-		std::string img_path_0 = GetCurrentPathUTF8() + "img_0.png";
-		std::string file_name_0 = GetImagePathUTF8() + std::to_string(m_share_wood_id) + "_" + std::to_string(0) + ".png";
+		std::string img_path_0 = GetCurrentPathUTF8() + "img_" + std::to_string(*pWndIndex - 1) + ".png";
+		std::string file_name_0 = GetImagePathUTF8() + std::to_string(m_share_wood_id) + "_" + std::to_string(*pWndIndex - 1) + ".png";
 		cv::Mat img_mat_0 = cv::imread(file_name_0);
-		std::string img_path_1 = GetCurrentPathUTF8() + "img_1.png";
-		std::string file_name_1 = GetImagePathUTF8() + std::to_string(m_share_wood_id) + "_" + std::to_string(1) + ".png";
+		std::string img_path_1 = GetCurrentPathUTF8() + "img_" + std::to_string(*pWndIndex) + ".png";
+		std::string file_name_1 = GetImagePathUTF8() + std::to_string(m_share_wood_id) + "_" + std::to_string(*pWndIndex) + ".png";
 		cv::Mat img_mat_1 = cv::imread(file_name_1);
 		save_pos_image(img_path_0, img_path_1, img_mat_0, img_mat_1,
 			results1, results2, r_mat32, t_mat32);
